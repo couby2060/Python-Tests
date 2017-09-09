c6c5b9487c8841e1d31343a22f1c08c501bf37e9
johannes <johannes2175@gmail.com>
2017-09-09 22:32:17 +0200

    Remove Duplicates and Remove no Valid EMail Adresses

--- csv-script1.py
+++ csv-script1.py
@@ -1,20 +1,26 @@
 #from sys import argv #Warum funktioniert die Zeile mit dem Import nicht?
 import sys
 import csv
+import re
 
 def main():
+    pattern = re.compile("^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$")
     with open(sys.argv[1], 'r') as csvfile:
         my_file = csv.reader(csvfile, delimiter=';', quotechar='"')
+        emails = set()
         for row in my_file:
-            #print (row[4])
-            emails = set()
-            print (emails)
             if row[4] not in emails:
-                   #writer.writerow(row)
-                   emails.add(row[4])
-                   #print (row[4])
-                   #print (', '.join(row))
+                emails.add(row[4])
+                #print(', '.join(row))
+                if pattern.match(row[4]):
+                    emails.add(row[4])
+                    print(', '.join(row))
                    
                    
 if __name__ == "__main__":
     main()
+
+
+
+
+
